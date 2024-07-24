6e1c929f090606eb5e5d6800d783a2d46a3ff2f7
Alvito Dwi Reza <alvito0x56@gmail.com>
2024-07-24 10:08:16 +0700

    Add 3 other views (#16)
    Add verification done view
    Add blank profile view
    Add reminder popup

---
+++ MC3/View/Profile/BlankProfileView.swift
@@ -0,0 +1,56 @@
+//
+//  BlankProfileView.swift
+//  MC3
+//
+//  Created by Alvito Dwi Reza on 24/07/24.
+//
+
+import SwiftUI
+
+struct BlankProfileView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("BlankProfileImage")
+                    .padding()
+                
+                Text("Oops!\nYou Have Not Complete Your Dog's Profile")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 15.0)
+                    
+                
+                Text("To gain the trust of other dog owners, please provide your dog's details and verify its health.")
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 41.0)
+                    .padding(.vertical, 6.0)
+                
+                // Button
+                NavigationLink(
+                    destination: SelectBreedView(),
+                    label: {
+                        Text("Complete Now")
+                            .font(.system(size: 17))
+                            .fontWeight(.semibold)
+                            .frame(width: 200, height: 20)
+                            .padding(12)
+                            .background(Colors.tosca)
+                            .cornerRadius(30)
+                            .foregroundColor(.white)
+                            .padding(18)
+                    }
+                )
+            }
+            
+            .navigationTitle(Text("Profile"))
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
+    }
+}
+
+#Preview {
+    BlankProfileView()
+}
---
+++ MC3/View/ReminderPopUp/ReminderPopUpView.swift
@@ -0,0 +1,100 @@
+//
+//  ReminderPopUpView.swift
+//  MC3
+//
+//  Created by Alvito Dwi Reza on 24/07/24.
+//
+
+import SwiftUI
+
+struct ReminderPopUpView: View {
+    @Binding var isActive: Bool
+
+    let title: String
+    let message: String
+    let buttonTitle: String
+    let action: () -> ()
+    @State private var offset: CGFloat = 1000
+
+    var body: some View {
+        ZStack {
+            Color(.black)
+                .opacity(0.5)
+                .onTapGesture {
+                    close()
+                }
+            VStack {
+                Image("MedicalRecordVerifImage")
+                Text(title)
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 3.0)
+                    
+                
+                Text(message)
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+                    
+                    
+
+                Button {
+                    action()
+                    close()
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(Colors.tosca)
+
+                        Text(buttonTitle)
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .padding()
+                }
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    close()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.title2)
+                        .fontWeight(.medium)
+                }
+                .tint(.black)
+                .padding()
+            }
+            .shadow(radius: 20)
+            .padding(30)
+            .offset(x: 0, y: offset)
+            .onAppear {
+                withAnimation(.spring()) {
+                    offset = 0
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+
+    func close() {
+        withAnimation(.spring()) {
+            offset = 1000
+            isActive = false
+        }
+    }
+}
+
+
+#Preview {
+    ReminderPopUpView(isActive: .constant(true),
+                      title: "Fill Your Dog's Information First",
+                      message: "To gain trust of other dog owners, please provide your dog's details and verify its health",
+                      buttonTitle: "Complete Now!",
+                      action: {})
+}
