1fb291e2419de6f6b410675832aa5ce7f1646952
Jeffri Lieca Handoyo <jhandoyo@student.ciputra.ac.id>
2024-07-25 11:19:15 +0700

    feat: Menambahkan fitur swipe pada for you & Image Picker Dog's information

--- MC3/View/ForYou/ForYouView.swift
+++ MC3/View/ForYou/ForYouView.swift
@@ -11,34 +11,7 @@ struct ForYouView: View {
     @EnvironmentObject var dogViewModel: DogViewModel
     var body: some View {
         NavigationStack {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .top) {
-                    ForEach(dogViewModel.showDogs) { dog in
-                        NavigationLink(
-                            destination: ProfileView(dog: dog),
-                            label: {
-                                ForYouCard(dog: dog)
-                            })
-                    } .listRowSeparator(.hidden)
-                }
-            }.padding(.bottom, 30)
-//            VStack(alignment: .leading, spacing: 20) {
-//                VStack{
-//                    Button(action: {
-//                        // ACTION
-//                    }) {
-//                        HStack {
-//                            Image(systemName: "map")
-//                            Text("See Dog Nearby") .font(.system(size: 17)).fontWeight(.semibold)
-//                        }
-//                        .frame(maxWidth: .infinity)
-//                        .padding(15)
-//                        .background(Colors.tosca)
-//                        .cornerRadius(15)
-//                        .foregroundColor(.white)
-//                    }
-//                }
-//            }
+            ForYouSwipe()
             .navigationBarTitle("For You", displayMode: .inline)
         }
     }
